89c14d09cec932107578d4247616c4598441d026
Braixtech <51983569+Braixtech@users.noreply.github.com>
2025-12-30 13:39:03 +0100

    Delete bookconservation directory

--- bookconservation/scripts/backup_database.php
+++
@@ -1,6 +0,0 @@
-<?php
-// scripts/backup_database.php
-$backupFile = 'backup-' . date('Y-m-d-His') . '.sql';
-$command = "mysqldump --user=root --password= --host=localhost arewa_conservation_db > storage/app/backups/{$backupFile}";
-system($command);
-echo "Backup created: {$backupFile}";
